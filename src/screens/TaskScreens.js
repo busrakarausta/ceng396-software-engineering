804e561dfbb6b10a045e726bcd641af4e2589b06
MerveDadas <mervedadas96@gmail.com>
2019-05-25 16:57:23 +0300

    Task Screen edited.

--- src/screens/TaskScreens.js
+++ src/screens/TaskScreens.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { Container, Header, Tab, Tabs, ScrollableTab } from "native-base";
-import ToDoScreen from "./src/screens/ToDoScreen";
-import InProgressScreen from "./src/screens/InProgressScreen";
-import CompletedScreen from "./src/screens/CompletedScreen";
+import ToDoScreen from "./ToDoScreen";
+import InProgressScreen from "./InProgressScreen";
+import CompletedScreen from "./CompletedScreen";
 export default class TabsScrollableExample extends Component {
   render() {
     return (
-      <Container>
-        <Header hasTabs />
+      <Container style={{ backgroundColor: "orange" }}>
         <Tabs renderTabBar={() => <ScrollableTab />}>
           <Tab heading="To Do">
             <ToDoScreen />
