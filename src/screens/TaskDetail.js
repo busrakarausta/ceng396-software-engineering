6d930bc9f1c3511482e216f3669f31ff67b80921
MerveDadas <mervedadas96@gmail.com>
2019-05-28 22:49:22 +0300

    Task Detail edited.

--- src/screens/TaskDetail.js
+++ src/screens/TaskDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, Dimensions } from "react-native";
+import { Text, Dimensions, View } from "react-native";
 import Modal from "react-native-modal";
 import TaskCard from "../components/TaskCard";
 import {
@@ -12,6 +12,7 @@ import {
   Body,
   Right
 } from "native-base";
+import { FlatList } from "react-native-gesture-handler";
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 
@@ -20,7 +21,19 @@ const sources = [
     name: "karaustabusra",
     deadline: "12/02/2020",
     desc: "You added a new task to Xamarin Basics.",
-    comment: "deneme comment"
+    comment: "deneme comment1"
+  },
+  {
+    name: "karaustabusra",
+    deadline: "12/02/2020",
+    desc: "You added a new task to Xamarin Basics.",
+    comment: "deneme comment2"
+  },
+  {
+    name: "karaustabusra",
+    deadline: "12/02/2020",
+    desc: "You added a new task to Xamarin Basics.",
+    comment: "deneme comment3"
   }
 ];
 
@@ -31,75 +44,58 @@ export default class TaskDetail extends React.Component {
   }
   render() {
     return (
-      <Modal
-        isVisible={true} //{this.props.isVisible}
-        hasBackdrop={true}
-        onBackdropPress={this.props.onBackdropPress}
-        style={{
-          borderRadius: 20,
-          marginTop: 20,
-          position: "absolute",
-          padding: 20,
-          //  backgroundColor: "rgba(255,255,255,0.8)",
-          alignSelf: "center",
-          width: width / 1.5,
-          height: height / 1.5,
-          backgroundColor: "pink"
-        }}
-      >
-        <Text
+      <View>
+        <Modal
+          isVisible={true} //{this.props.isVisible}
+          hasBackdrop={true}
+          onBackdropPress={this.props.onBackdropPress}
           style={{
-            fontWeight: "bold",
-            fontSize: 18,
-            textAlign: "left",
-            padding: 7
+            borderRadius: 20,
+            marginTop: 20,
+            position: "absolute",
+            padding: 20,
+            //  backgroundColor: "rgba(255,255,255,0.8)",
+            alignSelf: "center",
+            width: width / 1.5,
+            height: height / 1.5,
+            backgroundColor: "pink"
           }}
         >
-          Task Name:{sources.name}{" "}
-          <Text style={{ fontWeight: "normal" }}> Task 1 </Text>
-        </Text>
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 18,
-            textAlign: "left",
-            padding: 7
-          }}
-        >
-          Task Detail:{sources.desc}{" "}
-          <Text style={{ fontWeight: "normal" }}> I'm task description </Text>
-        </Text>
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 18,
-            textAlign: "left",
-            padding: 7
-          }}
-        >
-          Due Date:{sources.deadline}{" "}
-          <Text style={{ fontWeight: "normal" }}> I'm duedate </Text>
-        </Text>
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 18,
-            textAlign: "left",
-            padding: 7
-          }}
-        >
-          Comments {sources.comment} {"\n"}
-          <Icon active style={{ fontSize: 20 }} name="chatbubbles" />{" "}
-          <Text style={{ fontWeight: "normal" }}> We are comments </Text>
-          {"\n"}
-          <Icon active style={{ fontSize: 20 }} name="chatbubbles" />
-          <Text style={{ fontWeight: "normal" }}> We are comments </Text>
-          {"\n"}
-          <Icon active style={{ fontSize: 20 }} name="chatbubbles" />{" "}
-          <Text style={{ fontWeight: "normal" }}> We are comments </Text>
-          {"\n"}
-        </Text>
-      </Modal>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 18,
+              textAlign: "left",
+              padding: 7
+            }}
+          >
+            Task Name:{"sources.name"}{" "}
+            <Text style={{ fontWeight: "normal" }}> Task 1 </Text>
+          </Text>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 18,
+              textAlign: "left",
+              padding: 7
+            }}
+          >
+            Task Detail:{"sources.desc"}{" "}
+            <Text style={{ fontWeight: "normal" }}> I'm task description </Text>
+          </Text>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 18,
+              textAlign: "left",
+              padding: 7
+            }}
+          >
+            Due Date:{sources.deadline}{" "}
+            <Text style={{ fontWeight: "normal" }}> I'm duedate </Text>
+          </Text>{" "}
+        </Modal>
+      </View>
     );
   }
 }
